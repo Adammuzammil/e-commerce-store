7c3c035f8b8046dff8e44d11666a38c6682c8b6e
Adam Mohammed Muzammil <mm592678@gmail.com>
2024-09-04 15:47:02 +0530

    Initial commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,132 @@
+import { redis } from "../lib/redis.js";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { generateToken } from "../utils/generateToken.js";
+import { setCookies } from "../utils/setCookies.js";
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); //7 days
+};
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //create a new user
+    const user = new User({ name, email, password });
+
+    await user.save();
+
+    //step 1 - generate token
+    const { accessToken, refreshToken } = generateToken(user._id);
+
+    //step 2 - store refresh token
+    await storeRefreshToken(user._id, refreshToken);
+
+    //step 3 - set the cookies
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateToken(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+      setCookies(res, accessToken, refreshToken);
+
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refresh_token;
+    if (refreshToken) {
+      const decoded = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+      );
+      await redis.del(`refresh_token:${decoded.userId}`);
+    }
+
+    res.clearCookie("access_token");
+    res.clearCookie("refresh_token");
+    res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const refreshToken = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refresh_token;
+
+    if (!refreshToken) {
+      return res.status(401).json({ message: "No refresh token provided" });
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+    if (refreshToken !== storedToken) {
+      return res.status(401).json({ message: "Invalid refresh token" });
+    }
+
+    //if the token are same, then assign with new access token
+    const accessToken = jwt.sign(
+      { userId: decoded.userId },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "15m",
+      }
+    );
+
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.json({ message: "Token refreshed successfully" });
+  } catch (error) {
+    console.log("Error in refreshToken controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  login,
+  logout,
+  refreshToken,
+  signup,
+} from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/logout", logout);
+router.post("/login", login);
+router.post("/refresh-token", refreshToken);
+// router.post("/profile", getProfile);
+
+export default router;
